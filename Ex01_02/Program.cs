13ff8fd2775c4e8e795a5ea2acdaef918fc85b01
Yuval <yuvalla@mta.ac.il>
2024-04-27 00:11:58 +0300

    finished projects 2 and 3

--- Ex01_02/Program.cs
+++ Ex01_02/Program.cs
@@ -1,16 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 
 namespace Ex01_02
 {
-    class Program
+    public class Program
     {
         public static void Main()
         {
-            Console.WriteLine("Hello world");
+            PrintDiamond(0, 9);
+        }
+
+        public static void PrintDiamond(int i_numOfCurrentRow, int i_desiredNumOfRows)
+        {
+            if (i_numOfCurrentRow < i_desiredNumOfRows)
+            {
+                PrintDiamond(i_numOfCurrentRow + 1, i_desiredNumOfRows);
+                int spaces = Math.Abs(i_desiredNumOfRows / 2 - i_numOfCurrentRow);
+                Console.Write(new string(' ', spaces));
+                if (i_numOfCurrentRow < i_desiredNumOfRows / 2)
+                {
+                    PrintLine(2 * i_numOfCurrentRow + 1);
+                }
+                else
+                {
+                    PrintLine((i_desiredNumOfRows - i_numOfCurrentRow) * 2 - 1);
+                }
+            }
+        }
+
+        public static void PrintLine(int length)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
         }
     }
 }
--- Ex01_03/Program.cs
+++ Ex01_03/Program.cs
@@ -1,16 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
+using Ex01_02;
+
+
 
 namespace Ex01_03
 {
-    class Program
+    public class Program
     {
         public static void Main()
         {
-            Console.WriteLine("Hello world");
+            int numOfRows = GetUserInput();
+            Ex01_02.Program.PrintDiamond(0, numOfRows);
+        }
+
+        public static int GetUserInput()
+        {
+            bool successfullyParsed;
+            int numOfRows;
+
+            do
+            {
+                Console.WriteLine("Please enter the number of rows for the diamond: ");
+                string userInput = Console.ReadLine();
+                successfullyParsed = int.TryParse(userInput, out numOfRows);
+                if (!successfullyParsed)
+                {
+                    Console.WriteLine("Invalid input! \"" + userInput + "\" is not a whole number");
+                }
+            }
+            while (!successfullyParsed);
+
+            if (numOfRows % 2 == 0)
+            {
+                numOfRows++;
+            }
+
+            return numOfRows;
         }
     }
 }
