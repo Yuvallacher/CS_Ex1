a47547cc1bd212cb33e182718fffc2097d9acc16
Yuval <yuvalla@mta.ac.il>
2024-04-26 17:18:14 +0300

    Add project files.

---
+++ Ex01_01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Ex01_01
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world");
+            
+        }
+    }
+}
